a417898c6a96fec08d232c24321fb4631d466f03
louison pelletier <louison.pelletier@devoteam.com>
2025-05-21 16:00:47 +0200

    Go demo application

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<html><body><h1>SLSA Demo</h1></body></html>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	port := ":8084"
+	fmt.Println("Serveur démarré sur http://localhost" + port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println("Erreur lors du démarrage du serveur:", err)
+	}
+}
